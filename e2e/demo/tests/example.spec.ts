138fa8d73c5afb979b7532ac27b1bd97f6e1a1ab
Stev Vargas <stev.vargas@gmail.com>
2024-07-11 16:50:17 -0700

    Commit 1 7/11/24

--- e2e/demo/tests/example.spec.ts
+++ e2e/demo/tests/example.spec.ts
@@ -1,4 +1,18 @@
 import { testCase } from "helpers";
+import { examplePage } from "app/pages/example.page";
+import { exampleService } from "app/services/example.service";
 
-describe("Demo test", () => {
+describe("Demo", () => {
+    testCase({
+        ids: "C123",
+        title: "Example Test",
+        test() {
+            examplePage.navigate()
+                .then(() => exampleService.read.getData())
+                .then((response) => expect(response.isOkStatusCode).to.be.true)
+                .then(() => examplePage.actions.clickButton1("One"))
+                .then(() => examplePage.actions.clickButton1("Two"))
+                .then(() => examplePage.actions.clickButton2());
+        },
+    });
 });
